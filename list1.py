86ff95f9815f27c7d954d972b053ed7d37848d7d
rgrocock <russell.grocock@gmail.com>
2013-12-04 15:43:56 +0000

    initial check in of the string1 exercise from googles python tutorial

--- list1.py
+++ list1.py
@@ -56,8 +56,11 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return
+    return sorted(tuples, key=returnLast)
+
+
+def returnLast(word):
+    return word[-1]
 
 
 # Simple provided test() function used in main() to print
